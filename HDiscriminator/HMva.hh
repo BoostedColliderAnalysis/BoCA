8da444fb862255943d73d1adfb7d41765e7c2f85
Jan Hajer <jan@hajer.com>
2014-09-18 16:36:40 +0000

    add missing mva files

---
+++ HDiscriminator/HMva.hh
@@ -0,0 +1,34 @@
+# ifndef HMvaCpv_hh
+# define HMvaCpv_hh
+
+# include "HMvaBase.hh"
+
+/**
+ * @brief Prepares multivariant analysis
+ *
+ */
+class HMva : public HMvaBase
+{
+
+public:
+
+    /**
+    * @brief Constructor
+    *
+    */
+    HMva();
+
+    /**
+    * @brief Destructor
+    *
+    */
+    ~HMva();
+
+private:
+
+    void DefineVariables();
+
+
+};
+
+# endif
