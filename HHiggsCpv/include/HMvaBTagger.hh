585d611442cb68bf4e8171a077a05a2650b24bad
Jan Hajer <jan@hajer.com>
2014-11-19 08:08:58 +0000

    missing files

---
+++ HHiggsCpv/include/HMvaBTagger.hh
@@ -0,0 +1,56 @@
+# ifndef HMvaBTagger_hh
+# define HMvaBTagger_hh
+
+
+# include "HMva.hh"
+# include "HBranchHiggsCpv.hh"
+
+/**
+ *
+ * @brief Prepares multivariant analysis
+ *
+ */
+class hhiggscpv::HMvaBTagger : public hmva::HMva
+{
+
+public:
+
+    /**
+    * @brief Constructor
+    *
+    */
+    HMvaBTagger();
+
+    /**
+    * @brief Destructor
+    *
+    */
+    ~HMvaBTagger();
+
+    HReaderStruct CutLoop(const ExRootTreeReader * const, HReaderStruct&);
+
+    void ApplyBdt(const ExRootTreeReader * const, const std::string, const TFile * const, TMVA::Reader *);
+
+    float GetBdt(const hhiggscpv::HBTaggerBranch &Branch) const;
+
+protected:
+
+
+private:
+
+      HBTaggerBranch *Candidate;
+
+      void DefineVariables();
+
+      virtual inline std::string NameSpaceName() const {
+        return "HiggsCPV";
+      };
+
+    virtual inline std::string ClassName() const {
+        return "HMva";
+    };
+
+
+};
+
+# endif
