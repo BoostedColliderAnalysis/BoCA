8973acf8540d7728c73df2fe4e91fc01b98081d4
Li Ying-Ying <ylict@connect.ust.hk>
2015-07-15 15:00:25 +0800

    add FourTopTagger files

---
+++ HeavyHiggs/include/AnalysisNeutralFourTop.hh
@@ -0,0 +1,140 @@
+#pragma once
+
+#include "AnalysisHeavyHiggs.hh"
+
+#include "Doublet.hh"
+
+namespace analysis
+{
+
+/**
+ * @brief Namespace for the heavy higgs analyses
+ *
+ */
+
+namespace heavyhiggs
+{
+
+/**
+ *
+ * @brief HAnalysis subclass defining the HiggsCPV Analysis
+ *
+ * @author Jan Hajer
+ *
+ */
+template<typename Tagger>
+class AnalysisNeutralFourTop : public AnalysisHeavyHiggs<Tagger>
+{
+
+public:
+
+    AnalysisNeutralFourTop() {
+        this->tagger().set_analysis_name(ProjectName());
+	this->pre_cuts().SetPtLowerCut(Id::top,100);
+    }
+
+    void SetFiles(const Tag tag) final {
+        switch (tag) {
+        case Tag::signal :
+            this->NewFile(tag, Process::Htt);
+            break;
+        case Tag::background :
+            this->NewFile(tag, Process::tttt);
+            break;
+        }
+    }
+
+    std::string ProjectName() const final {
+        //        return  ProcessName() + "-" + ColliderName(collider_type()) + "-" + std::to_string(PreCut()) + "GeV-" + std::to_string(Mass()) + "GeV-Eta2.5";
+        return  ProcessName() + "-" + Name(this->collider_type()) + "-"+ std::to_string(this->Mass()) + "GeV";
+    };
+    
+
+private:
+
+    float SignalCrosssection() const {
+        switch (this->collider_type()) {
+        case Collider::LHC:
+            switch (this->Mass()) {
+            case 500:
+                return 25.528929726502543;
+            case 1000:
+                return 1.2783507034600217;
+            case 2000:
+                return 0.021907574118663196;
+            default:
+                Error("Signal Crosssection", "unhandled case");
+                return 1;
+            } ;
+        case Collider::FHC:
+        case Collider::LE:
+            switch (this->Mass()) {
+            case 500:
+                return 973.5805772514352;
+            case 1000:
+                return 123.02005671222373;
+            case 1500:
+                return 28.624904980998327;
+            case 2000:
+                return 9.485582085140349;
+            case 3000:
+                return 1.7540841248835577;
+            case 4000:
+                return 0.4851939478031553;
+            case 5000:
+                return 0.16696738296715652;
+            case 6000:
+                return 0.06731697180862359;
+            case 7000:
+                return 0.029372932414373627;
+            case 8000:
+                return 0.014255221936825225;
+            case 10000:
+                return 0.0038428602375120795;
+            case 12000:
+                return 0.0012219523755405267;
+            case 15000:
+                return 0.00026507004708327343;
+            case 20000:
+                return 0.000028218388829563033;
+            default:
+                Error("Signal Crosssection", "unhandled case");
+                return 1;
+            }
+        default:
+            Error("Signal Crosssection", "unhandled case");
+            return 1;
+        }
+    }
+
+    std::string ProcessName() const override {
+        return "NeutralFourTop";
+    }
+
+    int PassPreCut(const Event &event) const override {
+        Jets Particles = event.Partons().GenParticles();
+        Jets Tops = RemoveIfWrongAbsParticle(Particles, Id::top);
+//         if (Tops.size() != 2) {
+//             return 0;
+//         } else {
+//             if (Tops.at(0).pt() < this->PreCut()) return 0;
+//             if (Tops.at(1).pt() < this->PreCut()) return 0;
+//         }
+	
+        if (event.Hadrons().MissingEt().pt() < this->MissingEt()) return 0;
+        Jets Leptons = fastjet::sorted_by_pt(event.Leptons().leptons());
+        if (Leptons.size() < 2) return 0;
+        if (Leptons.at(0).pt() < this->LeptonPt()) return 0;
+	if (Leptons.at(1).pt() < this->LeptonPt()) return 0;
+	if (Leptons.at(0).user_info<JetInfo>().Charge() != Leptons.at(1).user_info<JetInfo>().Charge()) return 0;
+	
+        Jets jets = event.Hadrons().Jets();
+        if (jets.size() < 4) return 0;
+        return 1;
+    }
+
+};
+
+}
+
+}
---
+++ HeavyHiggs/source/MainNeutralFourTop.cpp
@@ -0,0 +1,34 @@
+#include "AnalysisNeutralFourTop.hh"
+#include "EventNeutralFourTopTagger.hh"
+
+int main()
+{
+    analysis::heavyhiggs::AnalysisNeutralFourTop<analysis::BottomTagger> bottom_analysis;
+    bottom_analysis.RunFast();
+
+    analysis::heavyhiggs::AnalysisNeutralFourTop<analysis::JetPairTagger> jet_pair_analysis;
+    jet_pair_analysis.RunFast();
+
+    analysis::heavyhiggs::AnalysisNeutralFourTop<analysis::WHadronicTagger> w_hadronic_analysis;
+    w_hadronic_analysis.RunFast();
+
+    analysis::heavyhiggs::AnalysisNeutralFourTop<analysis::WSemiTagger> w_semi_analysis;
+    w_semi_analysis.RunFast();
+
+    analysis::heavyhiggs::AnalysisNeutralFourTop<analysis::TopHadronicTagger> top_hadronic_analysis;
+    top_hadronic_analysis.RunFast();
+
+    analysis::heavyhiggs::AnalysisNeutralFourTop<analysis::TopLeptonicTagger> tops_leptonic_analysis;
+    tops_leptonic_analysis.RunFast();
+
+    analysis::heavyhiggs::AnalysisNeutralFourTop<analysis::heavyhiggs::HeavyHiggsSemiTagger> heavy_higgs_semi_analysis;
+    heavy_higgs_semi_analysis.RunFast();
+
+    analysis::heavyhiggs::AnalysisNeutralFourTop<analysis::heavyhiggs::SignatureNeutralTagger> signature_semi_analysis;
+    signature_semi_analysis.RunFast();
+
+    analysis::heavyhiggs::AnalysisNeutralFourTop<analysis::heavyhiggs::EventNeutralFourTopTagger> event_semi_analysis;
+    event_semi_analysis.RunFullSignificance();
+
+}
+
