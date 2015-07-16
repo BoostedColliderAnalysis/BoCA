567449afed750fe95fe57256b94d776740209d4d
Jan Hajer <jan@hajer.com>
2015-07-16 16:36:06 +0800

    signature

--- HeavyHiggs/source/SignatureNeutralTagger.cpp
+++ HeavyHiggs/source/SignatureNeutralTagger.cpp
@@ -16,58 +16,50 @@ SignatureNeutralTagger::SignatureNeutralTagger()
 
 int SignatureNeutralTagger::Train(const Event &event, PreCuts &pre_cuts, const Tag tag) const
 {
-    Info("event Tags");
-    float Mass = event.mass();
+    Info();
+    Jets higgs = heavy_higgs_semi_reader_.tagger().HiggsParticle(event, tag);
     std::vector<Sextet> sextets = heavy_higgs_semi_reader_.Multiplets(event);
-
-    Jets HiggsBoson = heavy_higgs_semi_reader_.tagger().HiggsParticle(event, tag);
-    sextets=BestMatches(sextets, HiggsBoson, tag);
-
-//     if (!sextets.empty()) Error("higgs sextets exists?");
-
+    sextets = BestMatches(sextets, higgs, tag);
 
     std::vector<Doublet> doublets = jet_pair_reader_.Multiplets(event);
+    Jets bottoms = jet_pair_reader_.tagger().BottomPair(event, tag);  //Write a function to get the jet pair
+    std::vector<Doublet> final_doublets;
 
-    std::vector<Doublet> Finaldoublets;
-    Jets Particles = jet_pair_reader_.tagger().BottomPair(event, tag);  //Write a function to get the jet pair
-
-    if (tag == Tag::signal) {
-        if (Particles.size() == 2) {
+    switch (tag) {
+    case Tag::signal :
+        if (bottoms.size() == 2) {
             for (const auto & doublet : doublets) {
-              if ((doublet.SingletJet1().delta_R(Particles.at(0)) < DetectorGeometry::JetConeSize() && doublet.SingletJet2().delta_R(Particles.at(1)) < DetectorGeometry::JetConeSize()) || (doublet.SingletJet1().delta_R(Particles.at(1)) < DetectorGeometry::JetConeSize() && doublet.SingletJet2().delta_R(Particles.at(0)) < DetectorGeometry::JetConeSize())) Finaldoublets.emplace_back(doublet);
+                if ((doublet.SingletJet1().delta_R(Particles.at(0)) < DetectorGeometry::JetConeSize() && doublet.SingletJet2().delta_R(Particles.at(1)) < DetectorGeometry::JetConeSize()) || (doublet.SingletJet1().delta_R(Particles.at(1)) < DetectorGeometry::JetConeSize() && doublet.SingletJet2().delta_R(Particles.at(0)) < DetectorGeometry::JetConeSize())) final_doublets.emplace_back(doublet);
             }
         }
+        break;
+    case Tag::background :
+        final_doublets = doublets;
+        break;
     }
-    if (tag == Tag::background) Finaldoublets = doublets;
 
     std::vector<Octet62> octets;
-    for (const auto & doublet : Finaldoublets) {
+    for (const auto & doublet : final_doublets) {
         for (const auto & sextet : sextets) {
-//             if (tag == Tag::signal && sextet.Jet().m() < Mass / 2)continue;
-//             if (tag == Tag::signal && sextet.Jet().m() > Mass * 3 / 2)continue;
             Octet62 octet(sextet, doublet);
-            if (octet.Overlap()) continue;   //FIXME why overlap severely
+            if (octet.Overlap()) continue;
             octet.SetTag(tag);
             octets.emplace_back(octet);
         }
     }
-//     if (!octets.empty())Error("octets exist");
 
     if (tag == Tag::signal && octets.size() > 1) {
-        Info("more than one event", octets.size());
+        Info(octets.size());
         std::sort(octets.begin(), octets.end());
         octets.erase(octets.begin() + 1, octets.end());
     }
-
     return SaveEntries(octets);
-
 }
 
 
 std::vector<Octet62> SignatureNeutralTagger::Multiplets(const Event &event, PreCuts &pre_cuts, const TMVA::Reader &reader) const
 {
-    Info("event Tags");
-
+    Info();
     std::vector<Doublet> doublets = jet_pair_reader_.Multiplets(event);
     std::vector<Sextet> sextets = heavy_higgs_semi_reader_.Multiplets(event);
     std::vector<Octet62> octets;
