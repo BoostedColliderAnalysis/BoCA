7205670d1db8d8f5988f1f9a389d95f84ba59918
Jan Hajer <jan@hajer.com>
2015-07-16 16:37:45 +0800

    minor change

--- HeavyHiggs/source/EventNeutralFourTopTagger.cpp
+++ HeavyHiggs/source/EventNeutralFourTopTagger.cpp
@@ -4,7 +4,8 @@
 namespace analysis
 {
 
-namespace heavyhiggs {
+namespace heavyhiggs
+{
 
 EventNeutralFourTopTagger::EventNeutralFourTopTagger()
 {
@@ -14,7 +15,7 @@ EventNeutralFourTopTagger::EventNeutralFourTopTagger()
 
 int EventNeutralFourTopTagger::Train(const analysis::Event &event, PreCuts &pre_cuts, const analysis::Tag tag) const
 {
-    Info("event Tags");
+    Info();
 
     Jets jets = bottom_reader_.Multiplets(event);
     Jets leptons = event.Leptons().leptons();
@@ -23,7 +24,7 @@ int EventNeutralFourTopTagger::Train(const analysis::Event &event, PreCuts &pre_
 
     std::vector<MultipletEvent<Octet62>> events;
     for (const auto & octet : octets) {
-      MultipletEvent<Octet62> octetevent(octet,event,jets);
+        MultipletEvent<Octet62> octetevent(octet, event, jets);
         octetevent.SetTag(tag);
         events.emplace_back(octetevent);
     }
@@ -32,23 +33,22 @@ int EventNeutralFourTopTagger::Train(const analysis::Event &event, PreCuts &pre_
 
 std::vector<MultipletEvent<Octet62>> EventNeutralFourTopTagger::Multiplets(const Event &event, const TMVA::Reader &reader) const
 {
-    Info("event Tags");
+    Info();
     std::vector<Octet62> octets = signature_neutral_reader_.Multiplets(event);
-
 
     Jets jets = bottom_reader_.Multiplets(event);
     Jets Leptons = event.Leptons().leptons();
 
     std::vector<MultipletEvent<Octet62>> multiplet_events;
     for (const auto & octet : octets) {
-        MultipletEvent<Octet62> multiplet_event(octet, event,jets);
-        multiplet_event.SetBdt(Bdt(multiplet_event,reader));
+        MultipletEvent<Octet62> multiplet_event(octet, event, jets);
+        multiplet_event.SetBdt(Bdt(multiplet_event, reader));
         multiplet_events.emplace_back(multiplet_event);
     }
 
     std::sort(multiplet_events.begin(), multiplet_events.end());
     if (multiplet_events.size() > 1) multiplet_events.erase(multiplet_events.begin() + 1, multiplet_events.end());
-    Info("event Number", multiplet_events.size(), jets.size());
+    Info(multiplet_events.size(), jets.size());
     return multiplet_events;
 }
 
