475bbcbbea895cf2e00445e2943d7e8d7e919abd
Jan Hajer <jan@hajer.com>
2016-04-06 01:23:34 +0800

    add missing typedef

---
+++ include/exroot/TypeDef.hh
@@ -0,0 +1,23 @@
+/**
+ * Copyright (C) 2015-2016 Jan Hajer
+ */
+#pragma once
+/**
+ * @brief ExRoot
+ *
+ */
+namespace exroot
+{
+using LHEFEvent = ::TRootLHEFEvent;
+using LHEFParticle = ::TRootLHEFParticle;
+using GenEvent = ::TRootGenEvent;
+using GenParticle = ::TRootGenParticle;
+using GenJet = ::TRootGenJet;
+using Event = ::TRootEvent;
+using MissingET = ::TRootMissingET;
+using Photon = ::TRootPhoton;
+using Electron = ::TRootElectron;
+using Muon = ::TRootMuon;
+using Tau = ::TRootTau;
+using Jet = ::TRootJet;
+}
