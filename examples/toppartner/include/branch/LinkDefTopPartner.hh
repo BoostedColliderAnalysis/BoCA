1dcf566e9356775d02eb315932b786cdaad7375c
Jan Hajer <jan@hajer.com>
2016-08-05 20:55:47 +0800

    merge top partner reorgainzation branch

--- examples/toppartner/include/branch/LinkDefTopPartner.hh
+++ examples/toppartner/include/branch/LinkDefTopPartner.hh
@@ -1,14 +1,14 @@
 #ifdef __MAKECINT__
-#pragma link C++ defined_in "../../../../../examples/TopPartner/include/branch/Complete.hh";
-#pragma link C++ defined_in "../../../../../examples/TopPartner/include/branch/HiggsTwoBody.hh";
-#pragma link C++ defined_in "../../../../../examples/TopPartner/include/branch/NewEvent.hh";
-#pragma link C++ defined_in "../../../../../examples/TopPartner/include/branch/NewEvent2.hh";
-#pragma link C++ defined_in "../../../../../examples/TopPartner/include/branch/NewEvent3.hh";
-#pragma link C++ defined_in "../../../../../examples/TopPartner/include/branch/NewEvent4.hh";
-#pragma link C++ defined_in "../../../../../examples/TopPartner/include/branch/SignatureSingleHadronic.hh";
-#pragma link C++ defined_in "../../../../../examples/TopPartner/include/branch/SignatureEffective.hh";
-#pragma link C++ defined_in "../../../../../examples/TopPartner/include/branch/SignatureSingle.hh";
-#pragma link C++ defined_in "../../../../../examples/TopPartner/include/branch/TopPartner.hh";
-#pragma link C++ defined_in "../../../../../examples/TopPartner/include/branch/Truth.hh";
-#pragma link C++ defined_in "../../../../../examples/TopPartner/include/branch/VetoTopPartner.hh";
+#pragma link C++ defined_in "../../../../../examples/toppartner/include/branch/Complete.hh";
+#pragma link C++ defined_in "../../../../../examples/toppartner/include/branch/HiggsTwoBody.hh";
+#pragma link C++ defined_in "../../../../../examples/toppartner/include/branch/NewEvent.hh";
+#pragma link C++ defined_in "../../../../../examples/toppartner/include/branch/NewEvent2.hh";
+#pragma link C++ defined_in "../../../../../examples/toppartner/include/branch/NewEvent3.hh";
+#pragma link C++ defined_in "../../../../../examples/toppartner/include/branch/NewEvent4.hh";
+#pragma link C++ defined_in "../../../../../examples/toppartner/include/branch/SignatureSingleHadronic.hh";
+#pragma link C++ defined_in "../../../../../examples/toppartner/include/branch/SignatureEffective.hh";
+#pragma link C++ defined_in "../../../../../examples/toppartner/include/branch/SignatureSingle.hh";
+#pragma link C++ defined_in "../../../../../examples/toppartner/include/branch/TopPartner.hh";
+#pragma link C++ defined_in "../../../../../examples/toppartner/include/branch/Truth.hh";
+#pragma link C++ defined_in "../../../../../examples/toppartner/include/branch/VetoTopPartner.hh";
 #endif
