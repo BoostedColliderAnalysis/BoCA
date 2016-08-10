ba234f36bffef076d90c9039f3c205687afd2a11
Jan Hajer <jan@hajer.com>
2016-08-10 16:42:44 +0800

    simplify more Events

--- examples/toppartner/include/tagger/EventSingleLeptonic.hh
+++ examples/toppartner/include/tagger/EventSingleLeptonic.hh
@@ -3,11 +3,10 @@
  */
 #pragma once
 
-#include "boca/tagger/Global.hh"
-#include "boca/multiplets/EventMultiplet.hh"
+#include "boca/tagger/Event.hh"
 
-#include "include/branch/Event.hh"
 #include "include/tagger/SignatureSingleLeptonic.hh"
+#include "include/branch/Event.hh"
 
 
 namespace toppartner
@@ -21,26 +20,14 @@ namespace tagger
  * @brief Prepares multivariant analysis
  *
  */
-class EventSingleLeptonic : public Tagger<EventMultiplet<Decuplet532>, branch::Event>
+class EventSingleLeptonic : public boca::tagger::Event<SignatureSingleLeptonic, Decuplet532, branch::Event>
 {
 
 public:
 
-    int Train(boca::Event const& event, PreCuts const& pre_cuts, Tag tag) override;
-
-    std::vector<EventMultiplet<Decuplet532>> Multiplets(boca::Event const& event, PreCuts const& pre_cuts, TMVA::Reader const& reader) override;
-
     std::string Name() const override;
 
     latex::String LatexName() const override;
-
-private:
-
-    std::vector<EventMultiplet<Decuplet532>> Events(boca::Event const& event, std::function<EventMultiplet<Decuplet532>(boca::EventMultiplet<Decuplet532> &)> const& function);
-
-    Reader<SignatureSingleLeptonic> signature_reader_;
-
-    Reader<boca::tagger::Global> global_reader_;
 
 };
 
