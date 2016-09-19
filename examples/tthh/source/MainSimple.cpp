5a8a0031bc6cef7afb89028dbff53ec9d9ba1fab
Jan Hajer <jan@hajer.com>
2016-09-19 19:43:40 +0800

    call the new taggers in the main file

--- examples/tthh/source/MainSimple.cpp
+++
@@ -1,9 +0,0 @@
-#include "tthh/Analysis.hh"
-#include "tthh/tagger/Global.hh"
-
-int main(){
-//   Analysis<boca::standardmodel::tagger::Bottom> bottom;
-//   bottom.Run(boca::Output::normal);
-  tthh::Analysis<tthh::tagger::Global> global;
-  global.Run(boca::Output::efficiency);
-}
