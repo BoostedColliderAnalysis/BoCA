b26e5b87267070a1890c9d3b4023b1ce9655c068
Jan Hajer <jan@hajer.com>
2016-10-14 18:44:32 +0800

    add copy constructor to Row

--- boca/latex/Row.hh
+++ boca/latex/Row.hh
@@ -19,6 +19,8 @@ public:
 
     Row(std::string const& cell);
 
+    Row(Row const& row) : row_(row.row_.str()),  first_(row.first_){}
+
     void Initialize();
 
     template <typename... Arguments>
