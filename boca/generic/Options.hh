fc4faf8a54bc6b220c65e7a57a0e3de17be502ba
Jan Hajer <jan@hajer.com>
2016-10-14 18:06:01 +0800

    harden constructor of Options

--- boca/generic/Options.hh
+++ boca/generic/Options.hh
@@ -7,6 +7,20 @@ class TString;
 namespace boca
 {
 
+namespace detail
+{
+
+template <typename>
+struct IsBool : std::false_type {};
+
+template <>
+struct IsBool<bool> : std::true_type {};
+
+template<typename Value_>
+using OnlyIfNotBool = typename std::enable_if < !IsBool<Value_>::value >::type;
+
+}
+
 /**
 * @ingroup Generic
 * @brief Key-value-pair options
@@ -19,28 +33,32 @@ public:
 
     Options();
 
-    Options(std::string const& string, bool do_it = true);
+    Options(std::string const &string, bool do_it = true);
 
-    template<typename Value>
-    Options(std::string const& string, Value value) {
+    template<typename Value_>
+    Options(std::string const &string, Value_ value)
+    {
         Add(string, value);
     }
 
-    template<typename Value>
-    Options(std::string const& string, Value value, std::string const& unit) {
+    template<typename Value_, typename = detail::OnlyIfNotBool<Value_>>
+    Options(std::string const &string, Value_ value, std::string const &unit)
+    {
         Add(string, value, unit);
     }
 
-    void Add(std::string const& string, bool do_it = true);
+    void Add(std::string const &string, bool do_it = true);
 
-    template<typename Value>
-    void Add(std::string const& string, Value value) {
+    template<typename Value_>
+    void Add(std::string const &string, Value_ value)
+    {
         Separator();
         options_ << string << "=" << value;
     }
 
-    template<typename Value>
-    void Add(std::string const& string, Value value, std::string const& unit) {
+    template<typename Value_>
+    void Add(std::string const &string, Value_ value, std::string const &unit)
+    {
         Separator();
         options_ << string << "=" << value << unit;
     }
