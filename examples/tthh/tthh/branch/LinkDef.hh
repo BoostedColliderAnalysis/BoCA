c9ae480c60df47cfe2d720b0f601dc00aa708997
Jan Hajer <jan@hajer.com>
2016-10-19 19:56:31 +0800

    add missing linkdef

--- examples/tthh/tthh/branch/LinkDef.hh
+++ examples/tthh/tthh/branch/LinkDef.hh
@@ -1,5 +1,6 @@
 #ifdef __MAKECINT__
 #pragma link C++ defined_in "../../../../../examples/tthh/tthh/branch/Global.hh";
 #pragma link C++ defined_in "../../../../../examples/tthh/tthh/branch/Higgs.hh";
+#pragma link C++ defined_in "../../../../../examples/tthh/tthh/branch/HiggsSemiLeptonic.hh";
 #pragma link C++ defined_in "../../../../../examples/tthh/tthh/branch/Signature.hh";
 #endif
